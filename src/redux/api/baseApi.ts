b4b4f93703b2d090bb4e9ee3b8ecd1d8e48c4bc1
sayed <sayedhasan019283@gmail.com>
2024-07-13 23:44:55 +0600

    add final code

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const baseApi = createApi({
   reducerPath: 'baseApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://sports-product-assignment-4-backend.vercel.app',
+    baseUrl: 'https://sports-product-assignment-4-backend.vercel.app/api/v1',
     // https://sports-product-assignment-4-backend.vercel.app
     credentials: 'include',
   }),
